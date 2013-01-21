827f6c99d8203bf977649ad642176eaeb377f70e
Wilson Mendes <willmendesneto@gmail.com>
2013-01-21 15:51:49 -0300

    Update .travis.yml for clone Codeigniter Repository for Travis.ci integration
    Signed-off-by: Wilson Mendes <willmendesneto@gmail.com>

--- application/third_party/CIUnit/bootstrap_phpunit.php
+++ application/third_party/CIUnit/bootstrap_phpunit.php
@@ -109,7 +109,7 @@ if ( ! defined('DS') ) {
     // Is the system path correct?
     if ( ! is_dir($system_path))
     {
-        exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
+        exit($system_path . "\nYour system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
     }
 
 /*
